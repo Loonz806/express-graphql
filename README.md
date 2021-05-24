23ff81eaf0dcb98c12efb8b4bfe6d11253f8d521
Lenny Peters <lpeters3@lululemon.com>
2021-05-24 14:36:49 -0700

    feat: 🎸 example finished
    next to integrate more API's

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-# Restful Conventions / Routing
+[![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
+[![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://github.com/prettier/prettier)
+[![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+# Express-GraphQL
+
+_learnings into GraphQL and what role it solves by making graphs over restful conventions/routing_
+
+### Restful Conventions / Routing Examples
+
+_restful meaning readable methods that you can assert actions on_
 
 /name POST Create a record
 /name GET Fetch all records
@@ -6,10 +16,13 @@
 /name/:id PUT Update details of user with id
 /name/:id DELETE Delete user with id
 
+_things can get complicated the more deeper and nested as you go_
 /users/23/posts POST Create a post associated with user 23
 /users/23/posts GET Fetch all posts created by user 23
 /users/23/posts/14 GET Fetch post 14 by user 23
 /users/23/posts/15 PUT Update post 15 by user 23
 /users/23/posts/18 DELETE Delete post 18 created by user 23
 
-## However these breakdown once we get into lots of http requests or super nested data
+### However these breakdown once we get into lots of http requests or super nested data
+
+Hence why using GraphQL perhaps can be a benefit
--- src/schema/schema.js
+++ src/schema/schema.js
@@ -1,50 +1,121 @@
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLSchema } = graphql;
-const _ = require("lodash");
+const {
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull,
+} = graphql;
+const axios = require("axios");
 
 // GraphQL needs an schema to work from
 
-const users = [
-  {
-    id: "23",
-    firstName: "Bill",
-    age: 20,
-  },
-  {
-    id: "47",
-    firstName: "Samantha",
-    age: 21,
-  },
-];
+// fields needs the arrow func to stop defined/circular deps
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    users: {
+      // We expect lists of users using this instead
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.id}/users`)
+          .then((response) => response.data);
+      },
+    },
+  }),
+});
 
 const UserType = new GraphQLObjectType({
   name: "User",
-  fields: {
-    id: {
-      type: GraphQLString,
+  fields: () => ({
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        // console.log('parentValue:', parentValue, 'args:', args);
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then((response) => response.data);
+      },
     },
-    firstName: {
-      type: GraphQLString,
-    },
-    age: {
-      type: GraphQLInt,
-    },
-  },
+  }),
 });
 
 // And requires a root query to jump off into
+// demonstrating connection to an outside server rather than static wiring.
+// adding siblings to make other connection queries for the RootQueryType
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
-  fields: {
+  fields: () => ({
     user: {
       type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`)
+          .then((response) => response.data);
+      },
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${args.id}`)
+          .then((response) => response.data);
+      },
+    },
+  }),
+});
+
+// Mutations ( adds, or modifies data )
+
+const mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    addUser: {
+      type: UserType,
       args: {
-        id: {
-          type: GraphQLString,
-        },
+        firstName: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) },
+        companyId: { type: GraphQLString },
+      },
+      resolve(parentValue, { firstName, age }) {
+        return axios
+          .post(`http://localhost:3000/users`, { firstName, age })
+          .then((response) => response.data);
+      },
+    },
+    editUser: {
+      type: UserType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLString) },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        companyId: { type: GraphQLString },
       },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+        return axios
+          .patch(`http://localhost:3000/users/${args.id}`, args)
+          .then((response) => response.data);
+      },
+    },
+    deleteUser: {
+      type: UserType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parentValue, { id }) {
+        return axios
+          .delete(`http://localhost:3000/users/${id}`)
+          .then((response) => response.data);
       },
     },
   },
@@ -52,4 +123,14 @@ const RootQuery = new GraphQLObjectType({
 
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation,
 });
+
+// You can make fragments in GraphQL so you can reduce copy/pasting
+/*
+fragment companyDetails on User {
+  id
+  name
+  description
+}
+*/
